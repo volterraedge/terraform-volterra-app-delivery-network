2f02c047aeed57aad779e8d9f4120eba1b458127
alexcohen <alexcohen@ves.io>
2022-02-16 16:23:07 -0500

    fix whitespace

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -24,9 +24,9 @@ resource "volterra_origin_pool" "this" {
 }
 
 resource "volterra_app_firewall" "this" {
-  name        = format("%s-waf", var.adn_name)
-  description = format("WAF in block mode for %s", var.adn_name)
-  namespace   = local.namespace
+  name                     = format("%s-waf", var.adn_name)
+  description              = format("WAF in block mode for %s", var.adn_name)
+  namespace                = local.namespace
   allow_all_response_codes = true
 }
 
