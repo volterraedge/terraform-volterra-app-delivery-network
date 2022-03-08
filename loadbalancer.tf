701433d7fb6ee9de4eaee3f77c1731040c4aad77
alexcohen <alexcohen@ves.io>
2022-03-08 12:41:35 -0500

    add option to disable blocking

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -28,7 +28,7 @@ resource "volterra_app_firewall" "this" {
   description              = format("WAF in block mode for %s", var.adn_name)
   namespace                = local.namespace
   allow_all_response_codes = true
-  blocking                 = true
+  blocking                 = var.blocking
 }
 
 resource "volterra_http_loadbalancer" "this" {
